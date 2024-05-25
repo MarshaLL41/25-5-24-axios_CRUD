180edfff648a32a704933f415ae03171e0d53d62
JIGNESH KALAMBE <kalambeji@gmail.com>
2024-05-25 11:18:03 +0530

    little changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const App = () => {
-    return <div>App</div>;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3001/posts`)
+            .then((res) => {
+                // console.log(res);
+                setData(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <>
+            <div>
+                <h1 className="text-center mt-4">AXIOS CRUD</h1>
+                <div className="w-75 mx-auto mt-5">
+                    {data?.map((val, ind, arr) => {
+                        return (
+                            <div className="text-center mt-3" key={ind}>
+                                <h5 className="">{val.title}</h5>
+                                <h5 className="">{val.author}</h5>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div></div>
+            </div>
+        </>
+    );
 };
 
 export default App;
---
+++ src/Axios_app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Axios_app = () => {
+    return (
+        <>
+            <div>
+                <h1>AXIOS CRUD</h1>
+            </div>
+        </>
+    );
+};
+
+export default Axios_app;
